2c72a0980e5122f12dac21ac0a5751d9c80e3530
Gaspor <gaspor991@gmail.com>
2023-11-17 17:12:14 -0300

    add: progress bar and json values

---
+++ donates.js
@@ -0,0 +1,10 @@
+const donates = {
+    "donates": [
+        {
+            "value": 657.00,
+            "name": "Inicial",
+            "data": "2023-11-17"
+        },
+        
+    ]
+}
--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const copyKeyFlavia = document.getElementById("copy_key_flavia");
 const textKeyMoh = document.getElementById("key_moh");
 const copyKeyMoh = document.getElementById("copy_key_moh");
 
+const progressBar = document.getElementById("progress_bar");
+const goal = (9000.00).toFixed(2);
+const valueNow = donates.donates.map(donate => donate.value).reduce((acc, amount) => acc + amount).toFixed(2);
+const percent = ((valueNow/goal) * 100).toFixed(2);
+
+const valueInProgress = document.getElementById("value_now");
+const labelValue = `R$${valueNow}/${goal}`;
+valueInProgress.textContent = labelValue;
+
+progressBar.style = `width: ${percent}%`;
+progressBar.textContent = percent + "%";
+
 copyKeyFlavia.addEventListener("click", () => {
     navigator.clipboard.writeText(textKeyFlavia.textContent);
 });
