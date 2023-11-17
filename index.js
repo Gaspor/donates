798390243ac25dfe6f7e3be7ceb5c1ceee9bedff
Gaspor <gaspor991@gmail.com>
2023-11-17 13:18:06 -0300

    add: key

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const textPhoneFlavia = document.getElementById("phone_flavia");
-const copyPhoneFlavia = document.getElementById("copy_phone_flavia");
+const textKeyFlavia = document.getElementById("key_flavia");
+const copyKeyFlavia = document.getElementById("copy_key_flavia");
 
-const textPhoneMoh = document.getElementById("phone_moh");
-const copyPhoneMoh = document.getElementById("copy_phone_moh");
+const textKeyMoh = document.getElementById("key_moh");
+const copyKeyMoh = document.getElementById("copy_key_moh");
 
-copyPhoneFlavia.addEventListener("click", () => {
-    navigator.clipboard.writeText(textPhoneFlavia.textContent);
+copyKeyFlavia.addEventListener("click", () => {
+    navigator.clipboard.writeText(textKeyFlavia.textContent);
 });
 
-copyPhoneMoh.addEventListener("click", () => {
-    navigator.clipboard.writeText(textPhoneMoh.textContent);
+copyKeyMoh.addEventListener("click", () => {
+    navigator.clipboard.writeText(textKeyMoh.textContent);
 });
