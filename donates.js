b67e9fbae412020219f00b5420df73866340fc9f
Ramon Costa <gaspor991@gmail.com>
2023-12-05 14:10:03 -0300

    Update donates.js

--- donates.js
+++ donates.js
@@ -1,7 +1,7 @@
 const donates = {
     "donates": [
         {
-            "value": 1900.20,
+            "value": 1970.90,
             "name": "Inicial",
             "data": "2023-11-17"
         },
